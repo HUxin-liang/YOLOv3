d7c820969d641daaa5f46b6fbaecc116de51caa6
huxinliang <huxinliang2019@ia.ac.cn>
2020-08-12 09:28:29 +0800

    add time_test

--- predictX.py
+++ predictX.py
@@ -4,6 +4,8 @@
 from yolo import YOLO
 from PIL import Image
 import os
+import time
+
 os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 yolo = YOLO()
 
@@ -15,6 +17,9 @@ while True:
         print('Open Error! Try again!')
         continue
     else:
+        start = time.time()
         r_image = yolo.detect_image(image)
+        end = time.time()
+        print('detection time: %.4f' %(end - start))
         r_image.save('imgX/3_result.jpg')
         # r_image.show()
