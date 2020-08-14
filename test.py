f5401a605719cb5a06bb7e03114e1b298e974405
huxinliang <huxinliang2019@ia.ac.cn>
2020-08-14 10:47:36 +0800

    add torchsummary/torchstat

--- test.py
+++ test.py
@@ -2,11 +2,17 @@ import torch
 from torchsummary import summary
 from nets.yolo3 import YoloBody
 from utils.config import Config
+from torchstat import stat
+import numpy as np
 import os
 if __name__ == "__main__":
     # 需要使用device来指定网络在GPU还是CPU运行
-    os.environ["CUDA_VISIBLE_DEVICES"] = '2'
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+    #     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
     config = {"model_params": {"backbone_name": "darknet_53"},"yolo": {"anchors": [[1,2,3],[2,3,4],[3,4,5]],"classes": 80}}
     m = YoloBody(config).to(device)
-    summary(m, input_size=(3, 416, 416))
+    # x = np.array([3,416,416],dtype=float)
+    # summary(m, input_size=(3, 416, 416))
+
+    stat(m, (3, 416, 416))
